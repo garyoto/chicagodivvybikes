52ae81c12dec22935474c35088374e9d35c538a5
gw <garretlewis@gmail.com>
2014-01-01 18:37:45 -0600

    script finally outputs a geojson file with proper properties. Still testing the final output.

--- script.py
+++ script.py
@@ -1,45 +1,25 @@
+#downloads latest json data from divvybikes.com and make a geojson file out of it.
+
 import json
 import urllib2
 
-#get the 300 stations from divvy in json format
+
 url = 'http://divvybikes.com/stations/json'
-
-#load json data
+features = []
 d = json.load(urllib2.urlopen(url))
+for idx, station in enumerate(d['stationBeanList']):
+    # avoid first entry
+    if idx:
+        features.append({'type': 'Feature', 
+                         'properties': station,
+                         'geometry': {'type': 'Point',
+                                      'coordinates': [station['longitude'], 
+                                                      station['latitude']]}})
 
-#extract the station list from original jason data
-newd = d['stationBeanList'][1:]
+result = {'type': 'FeatureCollection', 'features': features}
 
-
-#create new dict
-ndict = {}
-tdict = {}
-
-#add stationlist to dict
-ndict['features'] = newd
-
-#add "properties"
-ndict['properties'] = newd
-
-#add "type": "Feature" to ndict
-p = {'type': 'Feature'}
-tdict = tdict.update(p)
-
-#add geojson metadata
-ndict['type'] = 'FeatureCollection'
-
-
-#add "properties" as key to each nested dict
-
-
-#add k:v pair "type": "Feature" to each nested dict
-
-#Extract Latitude and Longitude from each bike station
-#append "geometry" : { "type": "Point", "coordinates": [ xx.xxxxxxxxx, yy.yyyyyyyyyy  ] } to list
-
-#output geojson file
 gj = open('divvybikestations.geojson', 'w')
-newdata = json.dumps(ndict, indent=4)
+newdata = json.dumps(result, indent=4)
 gj.write(newdata)
 gj.close()
 
